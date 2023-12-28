efdb6305d6bcb0691cd0332d9d1b34daf855cf55
Nazim Rzayev <nazimrzayev527@gmail.com>
2023-12-29 02:53:42 +0400

    Project Update Create types

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import { IGitHubUsers } from "./user";
+
+export const defaultUser: IGitHubUsers = {
+  login: "Nazim527",
+  id: 68445690,
+  avatar_url: "https://avatars.githubusercontent.com/u/68445690?v=4",
+  name: "Nazim Rzayev",
+  company: null,
+  blog: "nazimrzayev527@gmail.com",
+  location: "Azerbaijan, Baku",
+  bio: "Rzayev Nazim",
+  twitter_username: null,
+  public_repos: 14,
+  followers: 0,
+  following: 2,
+  created_at: "2020-07-17T20:32:19Z",
+};
