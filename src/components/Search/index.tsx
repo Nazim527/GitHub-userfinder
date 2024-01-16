056e1979beeed847bd41beb42d6b7c529a358132
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-01-17 01:08:06 +0400

    Search UI add

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './style.scss'
+import SearchIcon from '@mui/icons-material/Search';
+import { Button } from '..';
 
-const Search: React.FC = () => {
+
+interface ISearchProps {
+  hasError?: boolean;
+  onSubmit?: (text:string) => void
+}
+const Search: React.FC<ISearchProps> = ({hasError,onSubmit}) => {
   return (
-    <div>Search</div>
+    <form>
+      <div className="search">
+        <label htmlFor="search" className='label'>
+          <SearchIcon className='search_icon'/>
+        </label>
+        <input type="text"
+        className='textField'
+        name='username'
+        placeholder='Search Github username'/>
+
+        {hasError  && (
+          <div className='error'>No Result</div>
+        )}
+        <Button>Search</Button>
+      </div>
+    </form>
   )
 }
 
-export default Search
+export default Search  
