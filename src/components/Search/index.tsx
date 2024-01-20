7c4902602092590a39f12f31de4a00981cfec863
Nazim Rzayev <nazimrzayev527@gmail.com>
2024-01-21 02:55:36 +0400

    Update

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -6,12 +6,20 @@ import { Button } from '..';
 
 interface ISearchProps {
   hasError?: boolean;
-  onSubmit?: (text:string) => void
+  onSubmit: (text:string) => void
 }
 const Search: React.FC<ISearchProps> = ({hasError,onSubmit}) => {
-  const handleSubmit = () => {
+  const [userName, setUserName] = React.useState<string>("")
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
 
+    if(userName.trim()) {
+      onSubmit(userName)
+      setUserName("")
+    }
   }
+  console.log(userName);
+  
 
   return (
     <form onSubmit={handleSubmit}>
@@ -22,7 +30,9 @@ const Search: React.FC<ISearchProps> = ({hasError,onSubmit}) => {
         <input type="text"
         className='textField'
         name='username'
-        placeholder='Search Github username'/>
+        placeholder='Search Github username'
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}/>
 
         {hasError  && (
           <div className='error'>No Result</div>
--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -4,18 +4,31 @@ import { UserInfo, UserStat, UserTitle } from "./components";
 import { IGitHubUsers } from "../../types/user";
 
 const UserCard: React.FC<IGitHubUsers> = (props) => {
-  const {login, avatar_url, created_at, bio, name, followers, following, public_repos} = props
+  const {
+    login,
+    avatar_url,
+    created_at,
+    bio,
+    name,
+    followers,
+    following,
+    public_repos,
+    blog,
+    company,
+    location,
+    twitter_username
+  } = props;
   return (
     <div className="usercard">
-      <img
-        className="avatar"
-        src={avatar_url}
-        alt={login}
-      />
+      <img className="avatar" src={avatar_url} alt={login} />
       <UserTitle created_at={created_at} name={name} login={login} />
       <p className="user_bio">{bio || "This profile has no bio"}</p>
-      <UserStat public_repos={public_repos} followers={followers} following={following} />
-      <UserInfo />
+      <UserStat
+        public_repos={public_repos}
+        followers={followers}
+        following={following}
+      />
+      <UserInfo blog={blog} company={company} location={location} twitter_username={twitter_username} />
     </div>
   );
 };
